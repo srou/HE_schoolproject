04055002fd5918749d75fd96a9e3131d1c99860b
GitCuddi <shamsroumili@yahoo.fr>
2018-06-20 17:32:16 +0200

    knn

--- k_means_HE.py
+++ k_means_HE.py
@@ -2,7 +2,9 @@ import Pyfhel
 from Pyfhel import PyCtxt,PyPtxt,Pyfhel
 import numpy as np
 import pandas as pd 
+import time
 
+start = time.time()
 HE = Pyfhel()
 #Generate Key
 KEYGEN_PARAMS={ "p":2,        "r":32,
@@ -14,23 +16,32 @@ KEYGEN_PARAMS={ "p":2,        "r":32,
 print("  Running KeyGen with params:")
 print(KEYGEN_PARAMS)
 HE.keyGen(KEYGEN_PARAMS)
-print("  KeyGen completed")
+end=time.time()
+print("  KeyGen completed in ",start-end," sec." )
 
 #Load Dataset
-df=pd.read_csv("./Data_titanic/train.csv")
+df=pd.read_csv("./Data_titanic/train.csv", error_bad_lines=False)
 print("Number of cols : ",df.shape[1] )
 print("Number of rows : ",df.shape[0] )
 print("Colnames : ",df.columns.tolist())
 #Simplify Dataset (keep only relevant quantitative columns)
 df=df[['Survived','Pclass','Age','SibSp','Parch','Fare']]
 
+
 #Encrypt columns
+start = time.time()
 p1 = PyPtxt(df['Survived'].tolist(), HE)
 print("Encrypting 1st column: ")
 c1 = HE.encrypt(p1)
+end=time.time()
+print(start-end,"sec")
+
+start = time.time()
 p2 = PyPtxt(df['Pclass'].tolist(), HE)
 print("Encrypting 2nd column: ")
 c2 = HE.encrypt(p2)
+end=time.time()
+print(start-end,"sec")
 
 #sum of the first 2 columns
 c1 += c2
