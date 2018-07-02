aae17afb14331d541f2713d04278f1d5eac48778
GitCuddi <shamsroumili@yahoo.fr>
2018-07-02 11:47:31 +0200

    test is_smaller

--- test_scalar_product.py
+++ test_scalar_product.py
@@ -18,11 +18,15 @@ v1 = [range(8), range(3)]
 v2 = [range(1,9), range(1,4)]
 v3 = [range(8), range(3)]
 v4 = [range(8), range(3)]
+v5=[0,1,2]
+v6=[1,2,3]
 
 p1 = PyPtxt(v1, HE)
 p2 = PyPtxt(v2, HE)
 p3 = PyPtxt(v3, HE)
 p4 = PyPtxt(v4, HE)
+p5 = PyPtxt(v5, HE)
+p6 = PyPtxt(v6, HE)
 
 
 print("Encrypting v1: ", v1)
@@ -33,11 +37,15 @@ print("Encrypting v3: ", v3)
 c3 = HE.encrypt(p3)
 print("Encrypting v4: ", v4)
 c4 = HE.encrypt(p4)
-
+print("Encrypting v5: ", v5)
+c5 = HE.encrypt(p5)
+print("Encrypting v6: ", v6)
+c6 = HE.encrypt(p6)
 
 c1 += c4
 c2 *= c4
 c3 %= c4
+c5*=c6
 
 r1 = HE.decrypt(c1)
 r2 = HE.decrypt(c2)
@@ -46,4 +54,5 @@ r3 = HE.decrypt(c3)
 print("Encrypted sum v1 + v4: ", r1)
 print("Encrypted mult v2 * v4: ", r2)
 print("Encrypted scalar prod v3 * v4: ", r3)
+print("multiplication v5*v6 : ",HE.decrypt(c5))
 
