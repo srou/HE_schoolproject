35a6e36c1677080d4f267048bb6e2a9220ad6edb
GitCuddi <shamsroumili@yahoo.fr>
2018-07-09 15:28:52 +0200

    test is_smaller bitbybit

--- test_is_smaller_bitbybit.py
+++ test_is_smaller_bitbybit.py
@@ -5,30 +5,35 @@ import time
 import math
 import numpy as np
 
+##pb : how to choose n ??
 def is_smaller(x_bits,y_bits,HE,alpha=8,n=1000):
     #takes in input 2 encrypted number (st 0=< x,y < n) given in their binary form
     #coded on alpha bits
     #returns [1] iff y<x , [0] otherwise  (where [1]= encrypt(1))
     #HE is the Homomorphic Encryption scheme (Pyfhel object)
 
-    #Initialisation of same_prefix
+    #Initialisation of same_prefix and same_bit
     print("Initisalisation")
-    l=[1]+[0 for i in range(alpha-1)]
-    p=PyPtxt(l,HE)
-    same_prefix=HE.encrypt(p) 
-    #encrypt [1,1,1,..1]
-    p_1=PyPtxt([1 for i in range(alpha)],HE)   
+    p_1=PyPtxt([1], HE)
     c_1=HE.encrypt(p_1)
-
-    same_bit=c_1.copy(c_1)
-    same_bit=same_bit-((x_bits-y_bits)**2)
-
-    ##get same_prefix as a vector
-    res=(1-y_bits)*x_bits*same_prefix
-    res%=c_1  #sum of all the elements of res
-
-    print("res : ",HE.decrypt(res)) ##Dec(res) should be a vector [1,1,1,.. 1] iff x>y, else [0,...0]
+    same_prefix=[c_1]
+    same_bit=[]
+    res=(c_1-y_bits[0])*x_bits[0]
+    for i in range(alpha):                        #min(alpha,int(math.floor(math.log(n))+1))):
+        tmp1=c_1.copy(c_1)
+        same_bit.append(tmp1-((x_bits[i]-y_bits[i])**2))
+        tmp=c_1.copy(c_1)
+        #print("c_1 : ",HE.decrypt(c_1))
+        print("tmp : ",HE.decrypt(tmp))
+        for j in range(i+1):
+            print("same_bit : "+str(j),HE.decrypt(same_bit[j]))
+            tmp*=same_bit[j]
+        print("tmp : ",HE.decrypt(tmp))
+        same_prefix.append(tmp)
+        res+=(c_1-y_bits[i])*x_bits[i]*same_prefix[i]  ## peut etre un pb d'indice
+        print("res : ",HE.decrypt(res))
     return res
+
 
 start = time.time()
 HE = Pyfhel()
@@ -48,19 +53,24 @@ print("  KeyGen completed in "+str(end-start)+" sec." )
 
 #test is_smaller with integers 5 and 6
 x=6
-x_bits=[int(i) for i in list('{0:08b}'.format(x))] #int 5 as a list of 8 bits
+x_bits=[int(i) for i in list('{0:08b}'.format(x))] #int 6 as a list of 8 bits
+x_bits_enc=[]
 print("Encrypting "+str(x)+" in bits ",x_bits)
 start = time.time()
-x_bits_enc=HE.encrypt(x_bits)
-print("x_bits_enc = ",x_bits_enc.getIDs(),x_bits_enc.getLen())
+for i in x_bits:
+    x_bits_enc.append(HE.encrypt([i]))
 end=time.time()
 print(str(end-start)+" sec." )
 
 y=5
-y_bits=[int(i) for i in list('{0:08b}'.format(y))] #int 6 as a list of 8 bits
+y_bits=[int(i) for i in list('{0:08b}'.format(y))] #int 5 as a list of 8 bits
+y_bits_enc=[]
 print("Encrypting "+str(y)+" in bits.",y_bits)
-y_bits_enc=HE.encrypt(y_bits)
-#print("x_bits_enc = ",y_bits_enc.getIDs(),x_bits_enc.getLen())
+start = time.time()
+for i in y_bits:
+    y_bits_enc.append(HE.encrypt([i]))
+end=time.time()
+print(str(end-start)+" sec." )
 end=time.time()
 print(str(end-start)+" sec." )
 
