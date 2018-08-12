619b94151c3bc2777ff804dadad33bdfbcd15bf2
GitCuddi <shamsroumili@yahoo.fr>
2018-08-12 15:35:02 +0200

    test knn write

--- knn.py
+++ knn.py
@@ -8,7 +8,8 @@ from random import randint
 
 #test knn writing file
 
-f = open("results_knn2.txt", "a")
+f = open("results_knn3.txt", "a")
+f.write("knn_results")
 
 def encrypt_as_bits(x,alpha,HE,f=f):
     #takes in input a plaintext integer x =< 2^alpha -1
