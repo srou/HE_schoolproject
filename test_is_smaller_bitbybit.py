2a35b6ddfbe774ba33639ebbafd3fdbe20974362
GitCuddi <shamsroumili@yahoo.fr>
2018-08-25 14:59:36 +0200

    is_smaller

--- test_is_smaller_bitbybit.py
+++ test_is_smaller_bitbybit.py
@@ -119,7 +119,7 @@ def is_smaller_fast3(x_bits,y_bits,HE,alpha):
 #For a given number of bits alpha, this dict gives the smallest prime number greater than 2^alpha-1
 prime_dict={4:17, 5:37, 6:67, 7:131, 8:257, 9:521, 10:1031, 11:2053, 12:4099, 13:8209}
 
-L=30
+L=40
 filename="is_smaller_"+str(L)+".txt"
 f = open(filename, "a")
 
