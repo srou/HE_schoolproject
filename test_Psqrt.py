ff248914056a7e1d984bc53a7c3a86beeb437aa1
GitCuddi <shamsroumili@yahoo.fr>
2018-09-05 01:21:59 +0100

    psqrt

--- test_Psqrt.py
+++ test_Psqrt.py
@@ -56,6 +56,39 @@ def Psqrt(x,p,HE,f):
             res+=tmp
     return res
 
+def Psqrt2(x,p,HE,f):
+    #attempt to improve computation using the polynomialMult method
+    def coeffs_Psqrt(p):
+        #Returns the coefficients ri that will help compute the polynomial P_sqrt that interpolates the function f:x-->floor(sqrt(x)) on [p]
+        l1=range(0,p)
+        l2=[int(math.floor(math.sqrt(i))) for i in l1]
+        f.write("l2 : "+str(l2))
+        #find the coeffs ri (in Zp) that help construct the polynomial
+        r=[]
+        f.write("\n")
+        f.write("Computing coefficients of Psqrt") 
+        f.write("\n")
+        f.flush()
+        for i in range(p):
+            num=l2[i]
+            den=1
+            for j in range(p):
+                if i!=j:
+                    den*=i-j
+            tmp=(num*inv_modulo(den,p))%p
+            r.append(int(tmp))
+        return r
+    coeffs=coeffs_Psqrt(p)
+    #x encrypted as a single Ctxt
+    #0=< x =< p  , p prime
+    ctxt_poly=[]
+    coeffs_ctxt=[]
+    for i in range(p):
+        coeffs_ctxt.append(HE.encrypt(PyPtxt([coeffs[i]], HE)))
+        ctxt_poly.append(HE.encrypt(PyPtxt([x], HE)))
+    res=ctxt_poly.polynomialMult(coeff)
+    return res
+
 #For a given number of bits alpha, this dict gives the smallest prime number greater than 2^alpha-1
 prime_dict={4:17, 5:37, 6:67, 7:131, 8:257, 9:521, 10:1031, 11:2053, 12:4099, 13:8209}
 
