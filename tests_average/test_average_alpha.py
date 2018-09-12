83c0f0e99a96a8413a2180949c35f20cffb134ca
GitCuddi <shamsroumili@yahoo.fr>
2018-09-12 14:03:22 +0200

    psqrt

--- tests_average/test_average_alpha.py
+++ tests_average/test_average_alpha.py
@@ -126,8 +126,8 @@ prime_dict={4:17, 5:37, 6:67, 7:131, 8:257, 9:521, 10:1031, 11:2053, 12:4099, 13
 
 
 #alpha=args.alpha
-alpha=7
-L=40
+alpha=4
+L=30
 n=10
 filename="average_alpha"+str(alpha)+".txt"
 f = open(filename, "a")
